b58ce133323cf3bfa434fcb51909599e0bfbcc4f
Borna <borna.pavlic@yahoo.com>
2023-10-24 00:52:28 +0200

    feat: removes unused variables

--- main.js
+++ main.js
@@ -1,32 +1,21 @@
-/*const menuBtn = document.querySelector(".menu-btn");
-let menuOpen = false;
-menuBtn.addEventListener("click", () => {
-  if (!menuOpen) {
-    menuBtn.classList.add("open");
-    menuOpen = true;
-  } else {
-    menuBtn.classList.remove("open");
-    menuOpen = false;
-  }
-});*/
-const burger = document.querySelector(".burger");
-const navSlide = () => {
-  const navList = document.querySelector(".nav-links");
+'use strict';
 
-  const navLinks = document.querySelectorAll(".nav-links li");
-  burger.addEventListener("click", () => {
-    navList.classList.toggle("nav-active");
-    burger.classList.toggle("toggle");
+const burger = document.querySelector('.burger');
+const navSlide = () => {
+  const navList = document.querySelector('.nav-links');
+
+  burger.addEventListener('click', () => {
+    navList.classList.toggle('nav-active');
+    burger.classList.toggle('toggle');
   });
 };
 navSlide();
 
 const closingNavActive = () => {
-  const navActive = document.querySelector(".nav-active");
-  const navigationLinks = document.querySelector(".links");
-  navigationLinks.addEventListener("click", () => {
-    navigationLinks.classList.remove("nav-active");
-    burger.classList.remove("toggle");
+  const navigationLinks = document.querySelector('.links');
+  navigationLinks.addEventListener('click', () => {
+    navigationLinks.classList.remove('nav-active');
+    burger.classList.remove('toggle');
   });
 };
 closingNavActive();
